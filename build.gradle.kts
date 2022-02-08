8920907bb7eefd05f06c18ff542d42be95e05a32
Jeff Hutchison <jeff@jeffhutchison.com>
2022-02-08 11:14:42 -0500

    Convert Gradle build to Kotlin script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,11 @@
 plugins {
-    id 'application'
+    application
 }
 
 repositories {
     mavenCentral()
-    maven {
-        url "https://dl9vkdsodilqp.cloudfront.net/repo"
-    }
-    maven {
-        url "https://frcmaven.wpi.edu/artifactory/release"
-    }
+    maven(url = "https://dl9vkdsodilqp.cloudfront.net/repo")
+    maven(url = "https://frcmaven.wpi.edu/artifactory/release/")
 }
 
 dependencies {
@@ -21,11 +17,11 @@ dependencies {
     implementation("edu.wpi.first.wpiutil:wpiutil-java:2021.3.1")
 
     // used by DemoTargetData
-    implementation 'com.squareup.okio:okio:2.10.0'
-    implementation 'com.squareup.moshi:moshi:1.12.0'
+    implementation("com.squareup.okio:okio:2.10.0")
+    implementation("com.squareup.moshi:moshi:1.12.0")
     implementation("org.jetbrains:annotations:21.0.1")
 }
 
 application {
-    mainClass = "Main"
+    mainClass.set("Main")
 }
